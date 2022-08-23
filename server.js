0fa81ca1976af3f18dea8a07fcfbfc4e09fd057a
mnwana <mariellenwana@gmail.com>
2022-08-22 22:42:08 -0400

    add posta api request code body

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const exp = require("constants");
+const uuid = require("./helpers/uuid");
 
 const PORT = 3001;
 
@@ -29,6 +30,41 @@ app.get("/api/notes", (req, res) => {
 
 app.post("/api/notes", (req,res) => {
     const {title, text} = req.body;
+    if(title && text){
+    const newNote = {
+        title,
+        text, 
+        note_id: uuid(),
+    };
+
+    fs.readFile(path.join(__dirname, "/db/db.json"), (err,data) => {
+        if(err){
+            console.log(err);
+        }
+        else {
+            const parsedNotes = JSON.parse(data);
+            console.log(parsedNotes);
+            parsedNotes.push(newNote);
+            console.log(parsedNotes);
+            fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(parsedNotes, nuoll,4),(err) => 
+            err 
+            ? console.error(err)
+            : console.log(`Note ${newNote.title} has been saved`)
+            );
+        }
+    });
+
+    const response = {
+        status: "success",
+        body: newNote,
+    };
+
+    console.log(response);
+    res.json(reponse);
+};
+else {
+    res.json("Error in saving note");
+}
 })
 
 app.listen(PORT, () =>
