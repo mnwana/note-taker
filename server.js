7af098fb1431e58ac68e72e270fa4fe8e3fde903
mnwana <mariellenwana@gmail.com>
2022-08-22 21:30:47 -0400

    serve homepage

--- server.js
+++ server.js
@@ -11,3 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use(express.static("public"));
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
+
+app.listen(PORT, () => console.log(`App listening http://localhost:${PORT} 🚀`));
