dd17dece151263fefe705c7c8a528c15a83701b6
mnwana <mariellenwana@gmail.com>
2022-08-22 22:31:45 -0400

    add api requests

--- server.js
+++ server.js
@@ -8,10 +8,29 @@ const PORT = 3001;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
+// app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
 
-app.listen(PORT, () => console.log(`App listening http://localhost:${PORT} 🚀`));
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+app.get("/*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+  console.info(res.json(path.join(__dirname, "/db/db.json")));
+});
+
+app.post("/api/notes", (req,res) => {
+    const {title, text} = req.body;
+})
+
+app.listen(PORT, () =>
+  console.log(`App listening http://localhost:${PORT} 🚀`)
+);
