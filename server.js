08ef415a04e403d914f7490cf3994a569950da98
mnwana <mariellenwana@gmail.com>
2022-08-23 22:07:26 -0400

    refactor code

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 const exp = require("constants");
 const uuid = require("./public/utils/uuid");
-const noteData = require('./db/db.json');
+const noteData = require("./db/db.json");
 
 const PORT = process.env.PORT || 3001;
 
@@ -13,58 +13,63 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
-  
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-  });
 
+// get route for homepage to load index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+// get route for /notes to load notes.html
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
+// get route for /api/notes to return note data
 app.get("/api/notes", (req, res) => {
   res.json(noteData);
 });
 
-app.post("/api/notes", (req,res) => {
-    const {title, text} = req.body;
-    if(title && text){
+// post route for /api/notes to add new note text and title to noteData
+app.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
+  // if request body has note text and title, create a new note object with a randomly generated id
+  if (title && text) {
     const newNote = {
-        title,
-        text, 
-        id: uuid(),
+      title,
+      text,
+      id: uuid(),
     };
-
-    fs.readFile(path.join(__dirname, "/db/db.json"), (err,data) => {
-        if(err){
-            console.log(err);
-        }
-        else {
-            noteData.push(newNote);
-            fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(noteData, null,4),(err) => 
-            err 
-            ? console.error(err)
-            : console.log(`Note ${newNote.title} has been saved`)
-            );
-        }
-    });
-
+    // add newNote to noteData array
+    noteData.push(newNote);
+    // write noteData array as string to notes json file
+    fs.writeFile(
+      path.join(__dirname, "/db/db.json"),
+      JSON.stringify(noteData, null, 4),
+      (err) =>
+        // log response
+        err
+          ? console.error(err)
+          : console.log(`Note ${newNote.title} has been saved`)
+    );
+    // create success response
     const response = {
-        status: "success",
-        body: newNote,
+      status: "success",
+      body: newNote,
     };
-
+    // log success response
     console.log(response);
     res.json(response);
-}
-else {
+  }
+  // log note save error if error
+  else {
     res.json("Error in saving note");
-}
+  }
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-  });
+// get route for default to load index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
 app.listen(PORT, () =>
   console.log(`App listening http://localhost:${PORT} 🚀`)
