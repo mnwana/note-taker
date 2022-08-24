a7cbb01c24636dc2bc8bc910fec5d23fbbfea46f
mnwana <mariellenwana@gmail.com>
2022-08-23 20:47:21 -0400

    fix get *

--- server.js
+++ server.js
@@ -13,11 +13,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
-
+  
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
   });
-  
 
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
@@ -61,6 +60,10 @@ app.post("/api/notes", (req,res) => {
 else {
     res.json("Error in saving note");
 }
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.listen(PORT, () =>
